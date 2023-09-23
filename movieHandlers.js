8906511f8bf5fd8113c645f57fc05b3b79c7a194
othiel <othiel.leith@gmail.com>
2023-09-23 19:07:54 +0200

    expressdatabase

--- movieHandlers.js
+++ movieHandlers.js
@@ -1,47 +1,38 @@
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
 
-const getMovies = (req, res) => {
-  res.json(movies);
+const database = require("./database");
+const getUsers = (req, res) => {
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 };
 
-const getMovieById = (req, res) => {
+const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
-  const movie = movies.find((movie) => movie.id === id);
-
-  if (movie != null) {
-    res.json(movie);
-  } else {
-    res.status(404).send("Not Found");
-  }
+  database
+  .query("select * from users where id = ?", [id])
+  .then(([users]) => {
+    if (users[0] != null) {
+      res.json(users[0]);
+    } else {
+      res.status(404).send("Not Found");
+    }
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send("Error retrieving data from database");
+  });
 };
 
+
 module.exports = {
-  getMovies,
-  getMovieById,
+  getUsers,
+  getUserById,
+  
 };
